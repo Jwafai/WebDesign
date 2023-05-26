80ccd24a11fa7756d602a104fd7e9048318a9a22
Jwafai <112339857+Jwafai@users.noreply.github.com>
2023-05-26 10:26:36 +0300

    Add files via upload

---
+++ ClassActivites/Activity11/Activity11_Accordion/accordion.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $("#accordion").accordion({
+        collapsible: true, // Allow closing panels
+        active: false, // Start with all panels closed
+        heightStyle: "content" // Adjust the height of panels to content
+    });
+});
